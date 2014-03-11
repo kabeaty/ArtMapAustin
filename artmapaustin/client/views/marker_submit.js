a607c2e9c93da504dd8672a66aa30d70f4e6c9cd
Kathryn Beaty <kabeaty@gmail.com>
2014-03-11 16:16:46 -0500

    Add circle, marker, and popup to user location on markerSubmit page

--- artmapaustin/client/views/marker_submit.js
+++ artmapaustin/client/views/marker_submit.js
@@ -4,11 +4,27 @@ Meteor.Loader.loadJs('//cdn.leafletjs.com/leaflet-0.7.2/leaflet.js', Template.ma
 };
 
 Template.markerSubmit.rendered = function() {
-var map = L.map('map').locate({setView: true, watch: true});
-
+var map = L.map('map');
+map.setView([30.28, -97.73], 13);
 var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 })
+
+map.locate({setView: true, watch: true})
+  .on('locationfound', function(e) {
+    var marker = L.marker([e.latitude, e.longitude]).bindPopup("You are here");
+    var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
+      color: 'red',
+      fillColor: '#f03',
+      fillOpacity: 0.5
+    });
+  map.addLayer(marker);
+  map.addLayer(circle);
+})
+.on('locationerror', function(e) {
+  console.log('Error');
+  map = L.map('map').setView([30.28, -97.73], 13);
+});
 
 OpenStreetMap_Mapnik.addTo(map);
 
