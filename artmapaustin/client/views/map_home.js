f042671bc76f4dd9dbceea0392fea5255617e394
Kathryn Beaty <kabeaty@gmail.com>
2014-03-11 18:54:08 -0500

    Add Artspots collection, fixtures, and display markers for all added art on maphome

--- artmapaustin/client/views/map_home.js
+++ artmapaustin/client/views/map_home.js
@@ -4,12 +4,25 @@ Meteor.Loader.loadJs('//cdn.leafletjs.com/leaflet-0.7.2/leaflet.js', Template.ma
 };
 
 Template.maphome.rendered = function() {
-var map = L.map('map').setView([30.28, -97.73], 13);
+var map = L.map('map');
+map.setView([30.28, -97.73], 13);
 
 var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 })
 
 OpenStreetMap_Mapnik.addTo(map);
+
+var addToMap = function() {
+  var spots = Artspots.find().fetch();
+  var each, marker;
+  for (var i = 0; i < spots.length; i++) {
+    each = spots[i];
+    marker = new L.marker([each.latitude, each.longitude]);
+    marker.bindPopup(each.description).addTo(map);
+  }
+};
+
+addToMap();
 
 };
