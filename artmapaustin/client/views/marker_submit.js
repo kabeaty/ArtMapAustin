5066e0bfa6ff33e6edbf48160f17e6b5ba316253
Kathryn Beaty <kabeaty@gmail.com>
2014-03-12 22:23:27 -0500

    User can only add one marker, and marker is draggable

--- artmapaustin/client/views/marker_submit.js
+++ artmapaustin/client/views/marker_submit.js
@@ -10,15 +10,13 @@ var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x
   attribution: '&copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
 })
 
-map.locate({setView: true, watch: true})
+map.locate({setView: true})
   .on('locationfound', function(e) {
-    var marker = L.marker([e.latitude, e.longitude]).bindPopup("You are here");
     var circle = L.circle([e.latitude, e.longitude], e.accuracy/2, {
       color: 'red',
       fillColor: '#f03',
       fillOpacity: 0.5
     });
-  map.addLayer(marker);
   map.addLayer(circle);
 })
 .on('locationerror', function(e) {
@@ -28,4 +26,32 @@ map.locate({setView: true, watch: true})
 
 OpenStreetMap_Mapnik.addTo(map);
 
+var marker;
+map.on('click', function(e) {
+  if (!map.hasLayer(marker)) {
+  marker = L.marker([e.latlng.lat, e.latlng.lng], {draggable: true});
+  map.addLayer(marker);
+  marker.bindPopup("The art is here").openPopup();
+  } else {
+    $('#marker-true').text("You already added a marker. Drag to change its location.");
+  }
+  });
+
+Template.markerSubmit.events({
+  'submit form': function(e, template) {
+    e.preventDefault();
+
+    var artspot = {
+      description: $(e.target).find('[name=description]').val(),
+      latitude: marker.latlng.lat,
+      longitude: marker.latlng.lng
+    };
+
+    artspot._id = Artspots.insert(artspot);
+    Router.go('artspotPage', artspot);
+  }
+});
+
+
 };
+
