c36ca72fc21dbd431a8cfa42f2c2b3b5593d30ab
Kathryn Beaty <kabeaty@gmail.com>
2014-03-22 22:45:29 -0500

    Add Twitter button to artspotPage

--- artmapaustin/client/views/artspots_page.js
+++ artmapaustin/client/views/artspots_page.js
@@ -12,4 +12,6 @@ Template.artspotPage.rendered = function() {
     var mark = new L.marker([this.data.latitude, this.data.longitude]);
         mark.bindPopup(this.data.title).addTo(mapSpot);
 
+    !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+
 };
